cb9d57f1a42624d2b8673767808213ebe9f8e09e
Chris Cooper <Christopher.Cooper@vu.com>
2025-05-03 18:07:49 -0500

    split, store, print

--- counter.py
+++ counter.py
@@ -1,6 +1,20 @@
 def countDayOfTheWeek():
     # This first line is provided for you
+
+
     file_name = input("Enter a file name: ")
+
+    fhand = open(file_name)
+    di = {}
+
+    for line in fhand:
+        line = line.rstrip()
+        if line.startswith('From '):
+            abc = line.split()
+            day = abc[2]
+            di[day] = di.get(day, 0) + 1
+            
+    print(di)
 
 
 
